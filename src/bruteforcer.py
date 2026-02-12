af4b4f1180772b5bcad021f0af825686fe6d82b3
Adrien <adrien.djouder@gmail.com>
2026-02-12 17:40:28 +0100

    made a basic function

--- src/bruteforcer.py
+++ src/bruteforcer.py
@@ -1,3 +1,10 @@
 
-def XOR_bytes(data,key):
-    
+def XOR_bytes(data: bytes,key: bytes):
+    new_data= []
+    for i in range(len(data)):
+        key_index= i % len(key)
+        new_byte= data[i]^key[key_index]
+        new_data.append(new_byte)
+    return bytes(new_data)
+
+
