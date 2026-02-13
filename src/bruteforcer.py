469520dc458fe81f8d078f6d158f5f6f894e000e
Adrien <adrien.djouder@gmail.com>
2026-02-13 17:04:17 +0100

    the scoring function

--- src/bruteforcer.py
+++ src/bruteforcer.py
@@ -2,7 +2,7 @@
 #XOR data with a repeating key.
 def XOR_bytes(data: bytes,key: bytes):
     new_data = []
-    for i in range(len(data)):
+    for i in range (len(data)):
         key_index = i % len(key)
         new_byte = data[i]^key[key_index]
         new_data.append(new_byte)
@@ -19,6 +19,10 @@ def parse_key(key:str):
             known[i] = ord(key[i])
     return known, unknown
 
-
-key="AB?C"
-print(parse_key(key))
+#Score decrypted data based on printable characters.Higher score = more likely to be readable text
+def score_txt(data: bytes):
+    score = 0
+    for i in range (len(data)):
+        if 32 <= data[i] <= 126:
+            score += 1
+    return score
