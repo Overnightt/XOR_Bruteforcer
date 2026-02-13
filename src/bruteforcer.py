3b96f7dec73cc1958a6d3b849a271b45544b7ec8
Adrien <adrien.djouder@gmail.com>
2026-02-13 16:56:08 +0100

    the parsing function

--- src/bruteforcer.py
+++ src/bruteforcer.py
@@ -1,10 +1,24 @@
 
+#XOR data with a repeating key.
 def XOR_bytes(data: bytes,key: bytes):
-    new_data= []
+    new_data = []
     for i in range(len(data)):
-        key_index= i % len(key)
-        new_byte= data[i]^key[key_index]
+        key_index = i % len(key)
+        new_byte = data[i]^key[key_index]
         new_data.append(new_byte)
     return bytes(new_data)
 
+#Parse partial key with ? for unknown bytes
+def parse_key(key:str):
+    unknown = []
+    known = {}
+    for i in range (len(key)):
+        if key[i] == "?":
+            unknown.append(i)
+        else:
+            known[i] = ord(key[i])
+    return known, unknown
 
+
+key="AB?C"
+print(parse_key(key))
